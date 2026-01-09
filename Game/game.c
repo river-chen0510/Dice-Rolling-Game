253c80e3857f6b4f4a90061e3ed56118f4158ea1
River Chen <riverchen0510@gmail.com>
2026-01-08 21:04:18 -0800

    finished display results

--- Game/game.c
+++ Game/game.c
@@ -13,11 +13,83 @@ int computerWins = 0;
 int playerWins = 0;
 int ties = 0;
 
+int n = 0;
+
 //* arrays
 int player_roll[2];
 int computer_roll[2];
+int playerResult[100];
+int computerResult[100];
 
-// int rounds[];
+
+int rounds[100];
+
+int rollDice(){
+    rounds[n] = n;
+
+    //* Generate player number
+    for(int i = 0; i < 2; i++){
+        player_roll[i] = rand() % 6 + 1;
+    }
+
+    //* Print player roll
+    playerTotal = player_roll[0] + player_roll[1];
+    printf("You rolled: %d and %d. Total = %d \n", player_roll[0], player_roll[1], playerTotal);
+    playerResult[n] = playerTotal;
+
+    //* Generate computer number
+    for(int i = 0; i < 2; i++){
+        computer_roll[i] = rand() % 6 + 1;
+    }
+
+    //* Print computer roll
+    computerTotal =  computer_roll[0] + computer_roll[1];
+    printf("Computer rolled: %d and %d. Total = %d \n \n", computer_roll[0], computer_roll[1], computerTotal);
+    computerResult[n] = computerTotal;
+    
+    //* ifs
+    if(playerTotal > computerTotal){
+        printf("You won this round! \n \n \n");
+        playerWins++;
+    }
+
+    if(computerTotal > playerTotal){
+        printf("The computer won this round! \n \n \n");
+        computerWins++;
+    }
+
+    if(playerTotal == computerTotal){
+        printf("It's a tie! \n");
+        ties++;
+    }
+
+    gamesPlayed++;
+    n++;
+
+    // TODO: in main.c, put srand(time(0)) when calling rollDice();
+}
+
+int displayResults(){
+        int size = 1;
+
+        printf("Round Results: \n \n");
+
+        printf("Games played: %d \n \n", gamesPlayed);
+
+        for(int i = 0; i < gamesPlayed; i++){
+
+            rounds[i] = rounds[i] + 1;
+
+            printf("Round %d: ", rounds[i]);
+            printf("You: %d, ", playerResult[i]);
+            printf("Computer: %d \n", computerResult[i]);
+
+            }
+
+        printf("\n \n");
+
+        printf("Wins = You: %d. Computer: %d. Ties: %d. \n", playerWins, computerWins, ties);
+}
 
 int greeting(){
     while(1){
@@ -29,85 +101,29 @@ int greeting(){
         printf("3. Exit \n");
         
         printf("Please enter your selection: ");
-        scanf("%d", &selection);
+        scanf(" %d", &selection);
+
+        printf("\n \n");
+
+        //* Buffer clear
+        while (getchar() != '\n');
 
         if(selection == 1){
-            // TODO: Do testing once multi-file system is made
+            rollDice();
         }
 
         if(selection == 2){
+            displayResults();
             // TODO: Do testing once multi-file system is made
         }
 
         if(selection == 3){
             // TODO: Do testing once multi-file system is made
         }
-        else{
-            printf("Error! Please try again! \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n ");
-        }
 
 
     }
 }
-
-int rollDice(){
-    // int size = sizeof(rounds) / sizeof(rounds[0]);
-    // for(int i = 0; i < size; i++){
-    //     rounds[i] = i;
-    // }
-
-    //* Generate player number
-    for(int i = 0; i < 2; i++){
-        player_roll[i] = rand() % 6 + 1;
-    }
-
-    //* Print player roll
-    playerTotal = player_roll[0] + player_roll[1];
-    printf("You rolled: %d and %d. Total = %d \n", player_roll[0], player_roll[1], playerTotal);
-
-    //* Generate computer number
-    for(int i = 0; i < 2; i++){
-        computer_roll[i] = rand() % 6 + 1;
-    }
-
-    //* Print computer roll
-    computerTotal =  computer_roll[0] + computer_roll[1];
-    printf("Computer rolled: %d and %d. Total = %d \n \n", computer_roll[0], computer_roll[1], computerTotal);
-
-    
-    //* ifs
-    if(playerTotal > computerTotal){
-        printf("You won this round! \n");
-        playerWins++;
-    }
-
-    if(computerTotal > playerTotal){
-        printf("The computer won this round! \n");
-        computerWins++;
-    }
-
-    if(playerTotal == computerTotal){
-        printf("It's a tie! \n");
-        ties++;
-    }
-
-    gamesPlayed++;
-
-    // TODO: in main.c, put srand(time(0)) when calling rollDice();
-}
-
-int displayResults(){
-    // int size = sizeof(rounds) / sizeof(rounds[0]);
-
-    printf("Round Results: \n");
-
-    for()
-
-    printf("Games played: %d");
-    printf("Wins = You: %d. Computer: %d. Ties: %d.", playerWins, computerWins, ties);
-
-}
-
 
 //! Testing
 int main(){
